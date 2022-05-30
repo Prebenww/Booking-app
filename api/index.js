b38248dcd6844c3c76879bc0567c89bcfde88cfb
preben Wulff <prebenox@gmail.com>
2022-05-30 09:53:25 +0200

    first commit

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -32,6 +33,7 @@ mongoose.connection.on("connected", () => {
 });
 
 //middleware
+app.use(cors())
 app.use(cookieParser());
 app.use(express.json());
 
